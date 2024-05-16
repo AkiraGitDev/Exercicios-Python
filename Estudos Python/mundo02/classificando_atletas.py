35c75203457e2fdf020482a404c6ff27a6526fff
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2024-05-16 15:55:05 -0300

    Realização do exercício de atletas de natação.
    Exercício focado em estrutura condicional com mais de três possibilidades de ação (elif).

--- Estudos Python/mundo02/classificando_atletas.py
+++ Estudos Python/mundo02/classificando_atletas.py
@@ -6,3 +6,19 @@ A Confederação Nacional de Natação precisa de um programa que leia o ano de 
 – Até 25 anos: SÊNIOR
 – Acima de 25 anos: MASTER
 """
+import datetime
+
+ano_atual = datetime.datetime.today().year
+nascimento = int(input("Insira o ano de nascimento do atleta: "))
+idade = ano_atual - nascimento
+
+if idade <= 9:
+    print("Com {} anos de idade, a classificação do atleta é MIRIM.".format(idade))
+elif idade >= 10 and idade <= 14:
+    print("Com {} anos de idade, a classificação do atleta é INFANTIL.".format(idade))
+elif idade >= 15 and idade <= 19:
+    print("Com {} anos de idade, a classificação do atleta é JÚNIOR.".format(idade))
+elif idade >= 20 and idade <= 25:
+    print("Com {} anos de idade, a classificação do atleta é SÊNIOR.".format(idade))
+else:
+    print("Com {} anos de idade, a classificação do atleta é MASTER.".format(idade))
