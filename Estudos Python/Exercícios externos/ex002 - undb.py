302e502502b17f1d620c42b5dbf294fd8f50e35f
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2024-08-12 12:32:22 -0300

    Update ex002 - undb.py

--- Estudos Python/Exercícios externos/ex002 - undb.py
+++ Estudos Python/Exercícios externos/ex002 - undb.py
@@ -1,1 +1,25 @@
 """Escreva um programa que recebe como entrada um número inteiro positivo n. Em seguida, seu programa deve ler n números inteiros e adicioná-los numa lista. Por fim, seu programa receberá um número inteiro x e deve verificar se x pertence ou não à lista."""
+
+lista = []
+
+while True:
+    numero = int(input("Adicione um número à lista: "))
+    lista.append(numero)
+
+    escolha = input("Deseja adicionar outro número? (s/n): ").strip().lower()
+
+    while escolha != "s" and escolha != "n":
+        print("Escolha inválida. Tente novamente...")
+        escolha = input("Deseja adicionar outro número? (s/n): ").strip().lower()
+    
+    if escolha == "n":
+        break
+
+verificar = int(input("Insira o número que deseja verificar na lista: "))
+
+if verificar in lista:
+    print("O número {} está na lista!".format(verificar))
+else:
+    print("O número {} não está na lista...".format(verificar))
+
+print("A lista completa é: {}".format(lista))
