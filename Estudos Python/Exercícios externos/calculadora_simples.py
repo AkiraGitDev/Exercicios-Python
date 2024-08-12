7788ec2af913755d9231a4e11b6233585500d95e
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2024-08-12 12:41:18 -0300

    Update calculadora_simples.py

--- Estudos Python/Exercícios externos/calculadora_simples.py
+++ Estudos Python/Exercícios externos/calculadora_simples.py
@@ -2,8 +2,15 @@
 Desenvolva uma calculadora que possa realizar as operações básicas (adição, subtração, multiplicação, divisão). O usuário deve ser capaz de escolher a operação e fornecer os números de entrada.
 """
 print("-=" * 10, "CALCULADORA SIMPLES", "-=" * 10)
-print("OPÇÕES: 1) Soma, 2)Diferença, 3)Multiplicação e 4)Divisão.")
-operacao = input("Insira a opção de operação: ")
+print("OPÇÕES: 1) Soma, 2) Diferença, 3) Multiplicação e 4) Divisão.")
+
+operacao = int(input("Insira a opção de operação: "))
+
+# Verifica se a operação é válida
+while operacao not in [1, 2, 3, 4]:
+    print("Opção inválida. Tente novamente.")
+    operacao = int(input("Insira a opção de operação: "))
+
 n1 = float(input("Insira o 1º valor: "))
 n2 = float(input("Insira o 2º valor: "))
 
@@ -15,11 +22,10 @@ elif operacao == 2:
     print(f"A diferença dos valores {n1} e {n2} é {diferenca}.")
 elif operacao == 3:
     multiplicacao = n1 * n2
-    print(f"Os valores {n1} e {n2} multiplicados resulta {multiplicacao}.")
+    print(f"Os valores {n1} e {n2} multiplicados resulta em: {multiplicacao}.")
 elif operacao == 4:
-    divisao = n1 / n2
-    print(f"Os valores {n1} e {n2} dividos resultam {divisao}.")
-else:
-    while True:
-        print("Opção inválida. Tente novamente.")
-        operacao = input("Insira a opção de operação: ")
+    if n2 != 0:
+        divisao = n1 / n2
+        print(f"Os valores {n1} e {n2} divididos resultam em: {divisao}.")
+    else:
+        print("Erro: Divisão por zero não é permitida.")
