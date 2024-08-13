398ef452e5dca8fc8690555516d60af31b166ada
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2024-08-13 10:18:09 -0300

    Update cadastro_trabalhador.py

--- Estudos Python/mundo03/cadastro_trabalhador.py
+++ Estudos Python/mundo03/cadastro_trabalhador.py
@@ -1,2 +1,18 @@
 """Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar."""
 
+import datetime
+
+dados = {}
+dados['nome'] = input("Nome: ")
+nascimento = int(input("Ano de nascimento: "))
+dados['ctps'] = int(input("Carteira (0 = não possui): "))
+dados['idade'] = datetime.datetime.now().year - nascimento
+
+if dados['ctps'] != 0:
+    dados['contrato'] = int(input("Ano de contratação: "))
+    dados['salário'] = float(input("Salário: R$"))
+    dados['aposentadoria'] = (dados['contrato'] + 35) - datetime.datetime.now().year
+
+print("-=" * 30)
+for k, v in dados.items():
+    print(f'{k} tem o valor {v}.')
