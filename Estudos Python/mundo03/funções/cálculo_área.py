cd7b4549c2fe3a734f983cb8ba746005eb7eeb4f
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2024-08-16 10:49:17 -0300

    novo exercício

--- Estudos Python/mundo03/funções/cálculo_área.py
+++ Estudos Python/mundo03/funções/cálculo_área.py
@@ -1,1 +1,13 @@
 """Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno."""
+
+print("CONTROLE DE TERRENOS")
+print("-=" * 12)
+
+def cálculo_área(a, b):
+    área = a * b
+    print(f'A área de largura {a} e comprimento {b} é de {área}m².')
+
+# Programa Principal
+largura = float(input("Insira a largura do terreno (em metros): "))
+comprimento = float(input("Insira o comprimento do terreno (em metros): "))
+cálculo_área(largura, comprimento)
