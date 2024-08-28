cda408a887c6611e3a51ec3ae70c32a36798db69
Thiago Akira <139590126+AkiraGitDev@users.noreply.github.com>
2024-08-28 20:43:40 -0300

    Create função_fatorial.py

---
+++ Estudos Python/mundo03/funções/função_fatorial.py
@@ -0,0 +1,14 @@
+"""Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial."""
+
+def fatorial(num):
+    cálculo = 0
+    contador = num
+    while num > 0:
+        contador -= 1
+        cálculo *= num * (num - contador)
+    print(cálculo)
+
+
+
+
+fatorial(5)
